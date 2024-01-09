a80a72916db50c6061aa8a88bb64654d821de12c
reena_savireddy <reena_savireddy@thbs.com>
2024-01-09 16:19:24 +0530

    commited somewhat integration

--- frontend/frontend/project/project/settings.py
+++ frontend/frontend/project/project/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -117,9 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+# STATICFILES_FINDERS = [
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- frontend/frontend/project/ui/views.py
+++ frontend/frontend/project/ui/views.py
@@ -1,14 +1,45 @@
 from django.shortcuts import render
+import requests
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'welcome.html')
+    return render(request,'home.html')
 
 def login(request):
     return render(request,'login.html')
+result_text = ""
+def summarease(request):
+    if request.method == 'POST'and request.is_ajax():
+        data = request.POST.get("data")
+        line_length = request.POST.get("line_length", "")
 
-def summarize(request):
-    return render(request,'summarize.html')
+        if not all([data, line_length]):
+            return JsonResponse({'status': 'error', 'message': 'Invalid request'})
+
+        api_url = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+        api_token = "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"
+        headers = {"Authorization": f"Bearer {api_token}"}
+
+        def query(payload):
+            response = requests.post(api_url, headers=headers, json=payload)
+            return response.json()
+
+        try:
+            output = query({
+                "inputs": data,
+                "parameters": {"max_length": line_length},
+            })[0]
+
+            result_text = output["summary_text"]
+
+            return JsonResponse({'status': 'success', 'result': result_text})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
+
+    return render(request, 'summarease.html')
+
 
 def registration(request):
     return render(request,'registration.html')
@@ -19,3 +50,16 @@ def forgotpassword(request):
 
 def logout(request):
     return render(request,'welcome.html')
+
+def pdf1(request):
+    return render(request,'pdf1.html')
+
+def grammar(request):
+    return render(request,'grammar.html')
+
+def profile(request):
+    return render(request,'profile.html')
+
+def history(request):
+    return render(request,'history.html')
+
